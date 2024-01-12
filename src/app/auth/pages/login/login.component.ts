4d9ea2335b7d86f6bcc66df8d4bfab3b80a0700a
Luis <ariza.9485@gmail.com>
2024-01-12 15:08:41 -0500

    correcion de form

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -5,26 +5,21 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  public myForm: FormGroup = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['',[Validators.required, Validators.email]],
-  })
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
 
-  constructor(
-    private router: Router,
-    private fb : FormBuilder
-    ) {}
+  constructor(private router: Router, private fb: FormBuilder) {}
 
-    onSave():void {
-      console.log(this.myForm.value);
-    }
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 
   irARegister() {
     this.router.navigate(['register']);
   }
 }
-
